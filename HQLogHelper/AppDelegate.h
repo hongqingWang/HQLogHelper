6840655f7bab81355ba7b78deede43304f1a9fe6
hongqingWang <272338444@qq.com>
2016-12-15 21:06:33 +0800

    Initial Commit

---
+++ HQLogHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQLogHelper
+//
+//  Created by 王红庆 on 2016/12/15.
+//  Copyright © 2016年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQLogHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HQLogHelper
+//
+//  Created by 王红庆 on 2016/12/15.
+//  Copyright © 2016年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
